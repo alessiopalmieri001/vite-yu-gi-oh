ce8165fd8d6eaf70cadda56f2e3d65acbd33f4a8
Alessio Palmieri <146271148+alessiopalmieri001@users.noreply.github.com>
2024-01-22 16:45:54 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script >
+    import MyAppHeader from './components/MyAppHeader.vue';
+    import MyAppMain from './components/MyAppMain.vue';
+    import MyAppFooter from './components/MyAppFooter.vue';
+
+        export default{
+            data() {
+                return {
+
+                };
+            },
+            components: {
+                MyAppHeader,
+                MyAppMain,
+                MyAppFooter
+            },
+            methods: {
+
+            }
+        }
+</script>
+
+<template>
+    <h1>
+        Mia app
+    </h1>
+    <MyAppHeader />
+    <MyAppMain />
+    <MyAppFooter />
+</template>
+
+<style lang="scss" scoped>
+@use "assets/scss/main.scss" as *;
+@import "assets/scss/partials/reset.scss"
+</style>
